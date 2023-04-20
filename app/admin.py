a7754f1e79aa73b115166fb4a40b059bfc2930ba
Manikanta <Manikanta662001@gmail.com>
2023-04-20 11:16:58 +0530

    first commit

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from app.models import *
+
 # Register your models here.
-admin.site.register(Name)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 #from app.models import *
 from app.forms import *
+from django.http import HttpResponse
 # Create your views here.
 def django_form(request):
     form=NameForm()
     d={'form':form}
+    if request.method=='POST':
+        fd=NameForm(request.POST)
+        if fd.is_valid():
+            return HttpResponse(str(fd.cleaned_data))
     return render(request,'django_form.html',d)
